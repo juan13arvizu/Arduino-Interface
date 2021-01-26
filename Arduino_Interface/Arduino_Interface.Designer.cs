d737bd53122786aaeac18077bf715ea594784c77
juan13arvizu <71351101+juan13arvizu@users.noreply.github.com>
2021-01-25 21:42:59 -0600

    ARDUINO

---
+++ Arduino_Interface/Arduino_Interface.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Arduino_Interface
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.Conectar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.datosTxt_Box = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // Conectar
+            // 
+            this.Conectar.Location = new System.Drawing.Point(132, 116);
+            this.Conectar.Name = "Conectar";
+            this.Conectar.Size = new System.Drawing.Size(75, 23);
+            this.Conectar.TabIndex = 0;
+            this.Conectar.Text = "CONECTAR";
+            this.Conectar.UseVisualStyleBackColor = true;
+            this.Conectar.Click += new System.EventHandler(this.Conectar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // datosTxt_Box
+            // 
+            this.datosTxt_Box.Location = new System.Drawing.Point(56, 177);
+            this.datosTxt_Box.MinimumSize = new System.Drawing.Size(200, 150);
+            this.datosTxt_Box.Multiline = true;
+            this.datosTxt_Box.Name = "datosTxt_Box";
+            this.datosTxt_Box.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.datosTxt_Box.Size = new System.Drawing.Size(307, 161);
+            this.datosTxt_Box.TabIndex = 2;
+            this.datosTxt_Box.TextChanged += new System.EventHandler(this.datosTxt_Box_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(98, 379);
+            this.progressBar1.MarqueeAnimationSpeed = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(224, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Value = 10;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Arduino_Interface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 613);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.datosTxt_Box);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Conectar);
+            this.Name = "Arduino_Interface";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button Conectar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox datosTxt_Box;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Arduino_Interface/Arduino_Interface.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsApp7
+{
+    public partial class Arduino_Interface : Form
+    {
+        private SerialPort myport;
+        private string in_data;
+        public int progressbar;
+        public Arduino_Interface()
+        {
+            InitializeComponent();
+        }
+
+        private void Conectar_Click(object sender, EventArgs e)
+        {
+            myport = new SerialPort();
+            myport.BaudRate = 9600;
+            myport.PortName = textBox1.Text;
+            myport.Parity = Parity.None;
+            myport.DataBits = 8;
+            myport.StopBits = StopBits.One;
+            myport.DataReceived += myport_DataReceived;
+            //Linea de escritura -> Siguiente clase :B
+            try
+            {
+                myport.Open();
+                MessageBox.Show("Conexion Establecida! :D");
+                progressbar = int.Parse(in_data);
+                progressBar1.Increment(progressbar);
+                progressBar1.PerformStep();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void myport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            in_data = myport.ReadLine();
+            this.Invoke(new EventHandler(displayDataEvent));
+        }
+        private void displayDataEvent(object sender, EventArgs e)
+        {
+            datosTxt_Box.Text = in_data;
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void datosTxt_Box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            progressbar = int.Parse(in_data);
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Barbuttom_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
